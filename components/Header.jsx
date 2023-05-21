313f57ef7176b9b844723ec61a3dfa593295f9b1
masudfcs1 <masudfcs1@gmail.com>
2023-05-21 10:10:05 +0600

    css added

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 
 const Header = () => {
   const [mobileMenu, setmobileMenu] = useState(false);
-  return <div>Header</div>;
+  const [show, setshow] = useState("translate-y-0");
+  return (
+    <header>
+      <div
+        className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+      >
+        Header
+      </div>
+    </header>
+  );
 };
 
 export default Header;
