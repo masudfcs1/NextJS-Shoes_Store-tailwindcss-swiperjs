63376aa3a7932a5dd6d0ffce597a8a81a75c733a
masudfcs1 <masudfcs1@gmail.com>
2023-05-21 15:19:27 +0600

    added icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,9 +24,17 @@ const Header = () => {
         <Menu showCatmenu={showCatmenu} setshowCatmenu={setshowCatmenu} />
 
         <div className="flex items-center gap-2 text-black">
+          {/* Icon frvt */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative ">
+            <IoMdHeartEmpty className=" text-[15px] md:text-[20px]" />
+            <div className=" h-[15px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] ">
+              5
+            </div>
+          </div>
+          {/* icon cart */}
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative ">
             <BsCart className=" text-[15px] md:text-[20px]" />
-            <div className=" h-[14px] md:h-12 min-w-[14px] md:min-w-[18px] rouonded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] ">
+            <div className=" h-[15px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] ">
               5
             </div>
           </div>
