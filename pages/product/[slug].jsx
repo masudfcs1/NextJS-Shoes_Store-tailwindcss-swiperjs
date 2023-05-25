5dfb0858c490a68ae6d8ac2d682c052d20049774
masudfcs1 <masudfcs1@gmail.com>
2023-05-25 13:00:21 +0600

    added

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -1,3 +1,4 @@
+import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
 import Wrapper from "@/components/Wrapper";
 import React from "react";
 
@@ -9,7 +10,9 @@ const ProductDetails = () => {
           <div
             className=" w-full md:w-auto flex-[1.5] max-w-[500px]
         lg:max-w-full mx-auto lg:mx-0 "
-          ></div>
+          >
+            <ProductDetailsCarousel />
+          </div>
           <div className=" flex-[1] py-3 ">Right</div>
         </div>
       </Wrapper>
