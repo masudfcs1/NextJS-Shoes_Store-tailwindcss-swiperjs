6d99e5cd0260e0603576ce77bb5a0d0f1fc65773
masudfcs1 <masudfcs1@gmail.com>
2023-06-04 03:26:53 +0600

    cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -5,10 +5,16 @@ import Link from "next/link";
 
 const Cart = () => {
   return (
-    <div>
-      <div>
-        <Wrapper>Cart</Wrapper>
-      </div>
+    <div className=" w-full md:py-20">
+      <Wrapper>
+        {/* HEADING AND PARAGRAPH START */}
+        <div className=" text-center max-w-[800px] mx-auto mt-9 md:mt-0 ">
+          <div className=" text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Shopping Cart
+          </div>
+        </div>
+        {/* HEADING AND PARAGRAPH END */}
+      </Wrapper>
     </div>
   );
 };
