cc8948f16f0a0a020906eb596c3085abe34361ec
masudfcs1 <masudfcs1@gmail.com>
2023-06-14 15:15:32 +0600

    CartItem

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -13,14 +13,54 @@ const CartItem = () => {
           <div className=" text-lg md:text-lg font-semibold text-black/[0.8]">
             Jordan Retro 6 G
           </div>
-          {/* Product Subtitle */}
-          <div className=" text-sm md:text-md font-medium text-black/[0.5] block md:hidden ">
-            Men&apos;s Golf Shoes
-          </div>
 
           {/* Product Price */}
-          <div className=" text-sm md:text-md font-bold text-black/[0.5] mt-2">
+          <div className=" flex text-sm md:text-md font-bold text-black/[0.5] mt-2">
             MRP: $ 19695.00
+          </div>
+        </div>{" "}
+        {/* Product Subtitle */}
+        <div className=" text-md font-medium text-black/[0.5] hidden md:block  ">
+          Men&apos;s Golf Shoes
+        </div>
+        <div className="flex items-center justify-between mt-4">
+          <div className=" flex items-center gap-2 md:gap-10 text-black/[0.7] text-sm md:text-md ">
+            <div className="flex items-center gap-1">
+              <div className=" font-semibold ">Size:</div>
+
+              <select className=" hover:text-black ">
+                <option value="1">Uk 6</option>
+                <option value="2">Uk 6</option>
+                <option value="3">Uk 7</option>
+                <option value="4">Uk 7.5</option>
+                <option value="5">Uk 8</option>
+                <option value="6">Uk 8.5</option>
+                <option value="7">Uk 9</option>
+                <option value="8">Uk 9.5</option>
+                <option value="9">Uk 10</option>
+                <option value="10">Uk 10.5</option>
+                <option value="11">Uk 11</option>
+                <option value="">Uk </option>
+              </select>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className=" font-semibold ">Quantity:</div>
+
+              <select className=" hover:text-black ">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="">12 </option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
